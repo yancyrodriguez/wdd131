32e11185921b3642bbc4e2a72139f10dd2eaefdd
yancyrodriguez <yancy.veraliz.rodriguez@gmail.com>
2024-04-27 13:29:40 -0600

    change

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+// Current year
+
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+
+document.getElementById("lastModified").textContent = lastModified;
