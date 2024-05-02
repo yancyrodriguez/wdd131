6bb78b143bdedadf29a5d8588d5e019921f57a28
yancyrodriguez <yancy.veraliz.rodriguez@gmail.com>
2024-05-02 14:07:32 -0600

    commit

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+
+document.getElementById("lastModified").textContent = lastModified;
