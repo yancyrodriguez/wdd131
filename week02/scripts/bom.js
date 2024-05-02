b90fd370a923519447cf0c629d9265f81a6ab342
yancyrodriguez <yancy.veraliz.rodriguez@gmail.com>
2024-05-01 20:14:44 -0600

    commit

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -11,3 +11,11 @@ button.addEventListener('click', function() {
 });
 
 if (input.value.trim() !=='') {}
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+
+input.value = '';
+input.focus = '';
