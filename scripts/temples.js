1ae6b8d87bdf9d069219aae185819c3f588d6e24
yancyrodriguez <yancy.veraliz.rodriguez@gmail.com>
2024-05-02 21:31:01 -0600

    commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,22 @@
-const currentYear = new Date().getFullYear();
+// Current year
 
-document.getElementById("currentyear").textContent = currentYear;
+function updateFooter() {
+    const currentYear = new Date().getFullYear(); // Obtener el año actual
+    const lastModified = document.lastModified; // Obtener la última fecha de modificación
+    
+    //  span
+    document.getElementById("currentyear").textContent = currentYear;
+    document.getElementById("lastModified").textContent = lastModified;
+}
 
-const lastModified = document.lastModified;
+// Llamar a la función al cargar la página
+window.onload = updateFooter;
+//hamButton
 
-document.getElementById("lastModified").textContent = lastModified;
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
